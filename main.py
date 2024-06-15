d11fe1e950cbf79f75233151113ab94e3ece71fa
Arthur Gautier <120100522+SkowKyubu@users.noreply.github.com>
2024-06-15 16:26:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from coverletter import *
 from utils import *
 
-# Update your personal information if needed
-#update_personal_info()
+# Update your personal information if needed - you can uncomment this line after your first usage
+update_personal_info()
 
 # Update the company information
-#update_company_info()
+update_company_info()
 
 # Create a CoverLetter instance with the desired language ('en' for English, 'fr' for French)
 cover_letter = CoverLetter(language='en')
